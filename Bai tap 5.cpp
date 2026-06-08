dea7e8d653c1b6a51ea6762d00be9fb0a302fa50
HNM160206 <vainglory16022006@gmail.com>
2026-06-08 12:43:37 +0700

    bai tap tuan 14

---
+++ Bai tap 5.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int key;
+    Node *left, *right;
+    int height;
+};
+
+int height(Node *N) {
+    if (N == NULL) return 0;
+    return N->height;
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+Node* newNode(int key) {
+    Node* node = new Node;
+    node->key = key;
+    node->left = node->right = NULL;
+    node->height = 1;
+    return node;
+}
+
+Node* rightRotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
+Node* leftRotate(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+int getBalance(Node *N) {
+    if (N == NULL) return 0;
+    return height(N->left) - height(N->right);
+}
+
+// B1: Thêm nút vào AVL
+Node* insert(Node* node, int key) {
+
+    if (node == NULL)
+        return newNode(key);
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    node->height = 1 + max(height(node->left),
+                           height(node->right));
+
+    int balance = getBalance(node);
+
+    // LL
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    // RR
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    // LR
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // RL
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+// B2: Duyệt NLR
+void preorder(Node *root) {
+    if(root != NULL) {
+        cout << root->key << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main() {
+
+    int a[] = {32,51,27,83,96,11,45,75,66};
+
+    Node *root = NULL;
+
+    for(int i=0;i<9;i++)
+        root = insert(root,a[i]);
+
+    cout << "Preorder: ";
+    preorder(root);
+
+    return 0;
+}
